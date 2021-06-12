ec2c73b1fee26830c922cf68323bd913a675e690
Hee Hwang <heehcs@gmail.com>
2021-06-12 13:52:42 -0400

    add aug files

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,14 @@ pub struct OriData {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct HyperOriData {
+    pub id      : i32,
+    pub label   : String,
+    pub text    : String,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct AugData {
     pub text    : String,
     pub label   : String,
@@ -38,6 +46,21 @@ pub fn read_ori_jsonl(path: String) -> Result<Vec<OriData>, std::io::Error> {
 	    for line in BufReader::new(f).lines() {
 		let line = line.expect("couldn't get line");
 		let e: OriData = serde_json::from_str(&line)?;
+		v.push(e);
+	    }
+	    Ok(v)	    
+	},
+	Err(err) => Err(err)
+    }
+}
+
+pub fn read_hyper_ori_jsonl(path: String) -> Result<Vec<HyperOriData>, std::io::Error> {
+    let mut v = Vec::<HyperOriData>::new();
+    match File::open(path){
+	Ok(f) => {
+	    for line in BufReader::new(f).lines() {
+		let line = line.expect("couldn't get line");
+		let e: HyperOriData = serde_json::from_str(&line)?;
 		v.push(e);
 	    }
 	    Ok(v)	    
@@ -63,6 +86,7 @@ pub fn read_aug_jsonl(path: String) -> Result<Vec<AugData>, std::io::Error> {
 }
 
 pub fn write_aug_jsonl(v: Vec<AugData>, path: &str, num: i32) -> std::io::Result<()> {
+    println!("here");
     let _ = fs::create_dir_all(path);
     let filename = format!("{:0>5}{}", num, ".jsonl");
 
@@ -80,19 +104,31 @@ pub fn write_aug_jsonl(v: Vec<AugData>, path: &str, num: i32) -> std::io::Result
 
 
 pub fn label() {
-
-    let ori_file = "/home/heeh/Projects/augment/data/original/citation_intent/train.jsonl".to_string();
-    let aug_path = "/home/heeh/Projects/augment/data/aug_large/citation_intent/";
-    let lab_path = "/home/heeh/Projects/augment/data/aug_large_lab/citation_intent/";    
+    // let ori_file = "/home/heeh/Projects/rusty-wrangler/data/original/citation_intent/train.jsonl".to_string();
+    // let aug_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large/citation_intent/";
+    // let lab_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_lab/citation_intent/";    
+    // let ori_file = "/home/heeh/Projects/rusty-wrangler/data/original/hyperpartisan_news/train.jsonl".to_string();
+    // let aug_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large/hyperpartisan_news/";
+    // let lab_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_lab/hyperpartisan_news/";
+    let ori_file = "/home/heeh/Projects/rusty-wrangler/data/original/hyperpartisan_news/train.jsonl".to_string();
+    let aug_path = "/home/heeh/Projects/rusty-wrangler/data/aug_hard2/hyperpartisan_news/";
+    let lab_path = "/home/heeh/Projects/rusty-wrangler/data/aug_hard2_lab/hyperpartisan_news/";        
 
     println!("= Loading the data into OriData structs");
-    let ori_data = read_ori_jsonl(ori_file);
-    let ori = ori_data.unwrap();
+    // let ori_data = read_ori_jsonl(ori_file);
+    // let ori = ori_data.unwrap();
 
+    let hyper_ori_data = read_hyper_ori_jsonl(ori_file);
+    let ori = hyper_ori_data.unwrap();
+
+    
     println!("= Loading the data into AugData structs");
+
+
     for i in 0..ori.len() {
     	let filename = format!("{:0>5}{}", i, ".jsonl");
     	let aug_file = [aug_path,&*filename].join("");
+	println!("{:?}", aug_file);
         let aug_jsonl = read_aug_jsonl(aug_file);
     	let mut aug_jsonl = match aug_jsonl {
     	    Ok(v)    => v,
@@ -111,12 +147,31 @@ pub fn label() {
 
 
 pub fn collect(topk: usize) {
-    let ori_file = "/home/heeh/Projects/augment/data/original/citation_intent/train.jsonl".to_string();
-    let lab_path = "/home/heeh/Projects/augment/data/aug_large_lab/citation_intent/";
-    let top_path = "/home/heeh/Projects/augment/data/aug_large_top/citation_intent/";
-    let ori_data = read_ori_jsonl(ori_file);
-    let ori = ori_data.unwrap();
+    // let ori_file = "/home/heeh/Projects/rusty-wrangler/data/original/citation_intent/train.jsonl".to_string();
+    // let lab_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_lab/citation_intent/";
+    //let top_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_top/citation_intent/";
+    // let top_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_hard/citation_intent/";
 
+    // let ori_file = "/home/heeh/Projects/rusty-wrangler/data/original/hyperpartisan_news/train.jsonl".to_string();
+    // let lab_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_lab/hyperpartisan_news/";
+    // // let top_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_top/hyperpartisan_news/";
+    // let top_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_hard/hyperpartisan_news/";
+
+    let ori_file = "/home/heeh/Projects/rusty-wrangler/data/original/hyperpartisan_news/train.jsonl".to_string();
+    let lab_path = "/home/heeh/Projects/rusty-wrangler/data/aug_hard2_lab/hyperpartisan_news/";
+    // let top_path = "/home/heeh/Projects/rusty-wrangler/data/aug_large_top/hyperpartisan_news/";
+    let top_path = "/home/heeh/Projects/rusty-wrangler/data/aug_hard2_top/hyperpartisan_news/";
+    
+
+    
+    // let ori_data = read_ori_jsonl(ori_file);
+    // let ori = ori_data.unwrap();
+    let hyper_ori_data = read_hyper_ori_jsonl(ori_file);
+    let ori = hyper_ori_data.unwrap();
+
+
+
+    
     let _ = fs::create_dir_all(top_path);
     let filename = format!("top{:0>5}{}", topk, ".jsonl");
     let top_file = [top_path,&*filename].join("");
@@ -129,9 +184,17 @@ pub fn collect(topk: usize) {
         .unwrap();
 
     let bar = ProgressBar::new(ori.len() as u64);
-    for i in 0..ori.len() {
+
+    
+
+    // let hard = vec![8, 36, 44, 61, 112, 116, 138, 160, 201, 244, 299, 322, 540, 544, 547, 642, 656, 657, 692, 706, 719, 730, 736, 748, 752, 827, 857, 936, 950, 998, 1005, 1227, 1234, 1235, 1287, 1319, 1343, 1344, 1419, 1476, 1494, 1503, 1508, 1523, 1561, 1670];
+    //let hard = vec![56, 59, 106, 141, 147, 239, 355, 398, 411];
+    let hard = vec![0, 2, 3, 4, 5, 6, 7];
+    for x in hard.iter() {
+    //for i in 0..ori.len() {
 	bar.inc(1);
-    	let filename = format!("{:0>5}{}", i, ".jsonl");
+    	//let filename = format!("{:0>5}{}", i, ".jsonl");
+	let filename = format!("{:0>5}{}", *x, ".jsonl");
     	let lab_file = [lab_path,&*filename].join("");
         let lab_jsonl = read_aug_jsonl(lab_file);
     	let v = match lab_jsonl {
@@ -157,15 +220,32 @@ pub fn collect(topk: usize) {
 }
 
 pub fn merge(topk: usize) {
-    let ori_path    = "/home/heeh/Projects/augment/data/original/citation_intent/";
-    let ori_file    = "/home/heeh/Projects/augment/data/original/citation_intent/train.jsonl".to_string();
-    let top_path    = "/home/heeh/Projects/augment/data/aug_large_top/citation_intent/";
-    let merged_path = ["/home/heeh/Projects/augment/data/merged/citation_intent/".to_string(), format!("top{:0>5}/", topk)].join("");
+    // let ori_path    = "/home/heeh/Projects/rusty-wrangler/data/original/citation_intent/";
+    // let ori_file    = "/home/heeh/Projects/rusty-wrangler/data/original/citation_intent/train.jsonl".to_string();
+    // let top_path    = "/home/heeh/Projects/rusty-wrangler/data/aug_large_top/citation_intent/";
+    // let merged_path = ["/home/heeh/Projects/rusty-wrangler/data/merged/citation_intent/".to_string(), format!("top{:0>5}/", topk)].join("");
+    // let top_path    = "/home/heeh/Projects/rusty-wrangler/data/aug_large_hard/citation_intent/";
+    // let merged_path = ["/home/heeh/Projects/rusty-wrangler/data/merged_hard/citation_intent/".to_string(), format!("top{:0>5}/", topk)].join("");
+
+    let ori_path    = "/home/heeh/Projects/rusty-wrangler/data/original/hyperpartisan_news/";
+    let ori_file    = "/home/heeh/Projects/rusty-wrangler/data/original/hyperpartisan_news/train.jsonl".to_string();
+    // let top_path    = "/home/heeh/Projects/rusty-wrangler/data/aug_large_top/hyperpartisan_news/";
+    // let merged_path = ["/home/heeh/Projects/rusty-wrangler/data/merged/hyperpartisan_news/".to_string(), format!("top{:0>5}/", topk)].join("");
+    // let top_path    = "/home/heeh/Projects/rusty-wrangler/data/aug_large_hard/hyperpartisan_news/";
+    // let merged_path = ["/home/heeh/Projects/rusty-wrangler/data/merged_hard/hyperpartisan_news/".to_string(), format!("top{:0>5}/", topk)].join("");
+    
+    let top_path    = "/home/heeh/Projects/rusty-wrangler/data/aug_hard2_top/hyperpartisan_news/";
+    let merged_path = ["/home/heeh/Projects/rusty-wrangler/data/aug_hard2_merge/hyperpartisan_news/".to_string(), format!("top{:0>5}/", topk)].join("");
+
     
     let filename    = format!("top{:0>5}{}", topk, ".jsonl");
     let top_file    = [top_path,&*filename].join("");
-    let ori_data    = read_ori_jsonl(ori_file);
-    let ori         = ori_data.unwrap();
+
+    // let ori_data    = read_ori_jsonl(ori_file);
+    // let ori         = ori_data.unwrap();
+    let hyper_ori_data = read_hyper_ori_jsonl(ori_file);
+    let ori = hyper_ori_data.unwrap();
+
     let top_data    = read_aug_jsonl(top_file);
     let top         = top_data.unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,26 @@ use lib::merge;
 
 fn main() {
     //label();
+    // let acl_hard = vec![5,10,20,30,50,100,150,200,250,300,350,400,450,500,550,600];
+    let hyper_hard = vec![5,10,20,30,50,100,150,200,250, 300, 350, 400, 450, 500, 550, 600];
+    
     // for i in 1..=10 {
     // 	collect(i);
     // }
-//    merge(1);
-    for i in 2..=10 {
-    	merge(i);	
+    // for i in 1..=10 {
+    // 	merge(i);	
+    // }
+
+    // for x in hard.iter() {
+    // 	collect(*x);
+    // }
+    // for x in hard.iter() {
+    // 	merge(*x);
+    // }
+    // for x in hyper_hard.iter() {
+    // 	collect(*x);
+    // }
+    for x in hyper_hard.iter() {
+    	merge(*x);
     }
 }
